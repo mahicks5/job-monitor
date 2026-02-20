e26a004585d2a2cc2ba57d6e324d6f39a55cf5fe
Maxwell <skate1011rocks@gmail.com>
2026-02-19 20:02:54 -0700

    initial setup

---
+++ src/main/java/maxwellhicks/dev/job_monitor/JobMonitorApplication.java
@@ -0,0 +1,13 @@
+package maxwellhicks.dev.job_monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/maxwellhicks/dev/job_monitor/JobMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package maxwellhicks.dev.job_monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
