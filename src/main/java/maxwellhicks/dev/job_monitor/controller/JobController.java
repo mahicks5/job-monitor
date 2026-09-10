4e02c9a05641e51637a77aa9ef31b26e382bc2d2
Maxwell <skate1011rocks@gmail.com>
2026-09-09 20:34:37 -0600

    add company filter and styled UI

--- src/main/java/maxwellhicks/dev/job_monitor/controller/JobController.java
+++ src/main/java/maxwellhicks/dev/job_monitor/controller/JobController.java
@@ -1,10 +1,13 @@
 package maxwellhicks.dev.job_monitor.controller;
 
 import maxwellhicks.dev.job_monitor.repository.JobPostingRepository;
+import org.springframework.data.annotation.QueryAnnotation;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class JobController {
@@ -15,8 +18,12 @@ public class JobController {
     }
 
     @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("jobs", jobPostingRepository.findAll());
+    public String index(@RequestParam(required = false) String company, Model model) {
+        if (company != null && !company.isBlank()) {
+            model.addAttribute("jobs", jobPostingRepository.findByCompany(company));
+        } else {
+            model.addAttribute("jobs", jobPostingRepository.findAll());
+        }
         return "index";
     }
 
--- src/main/java/maxwellhicks/dev/job_monitor/repository/JobPostingRepository.java
+++ src/main/java/maxwellhicks/dev/job_monitor/repository/JobPostingRepository.java
@@ -3,8 +3,11 @@ package maxwellhicks.dev.job_monitor.repository;
 import maxwellhicks.dev.job_monitor.model.JobPosting;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface JobPostingRepository extends JpaRepository<JobPosting, Long> {
     Optional<JobPosting> findByExternalId(String externalId);
+
+    List<JobPosting> findByCompany(String company);
 }
